6fd0d5e934557b41cf351ad766cb50ad9244390e
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-18 16:26:56 +0200

    detail informations

--- src/components/GoBackBtn/GoBackBtn.jsx
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -1,3 +1,5 @@
-export const GoBackBtn = () => {
-  return <h3>GoBackBtn</h3>;
+import { Link } from 'react-router-dom';
+
+export const GoBackBtn = ({ children }) => {
+  return <Link to="/">{children}</Link>;
 };
